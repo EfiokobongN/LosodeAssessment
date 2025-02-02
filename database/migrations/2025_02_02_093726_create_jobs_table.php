1bf48ee09164c71481795e41bceaf76be1788bf8
EfiokobongN <mailpreciousnelson@gmail.com>
2025-02-02 10:24:38 +0000

    project creation and database migration setup

---
+++ database/migrations/2025_02_02_093726_create_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('job_title');
+            $table->string('company_name');
+            $table->text('job_description');
+            $table->string('location');
+            $table->string('employment_status');
+            $table->string('salary_range');
+            $table->date('submission_deadline');
+            $table->string('job_sector');
+            $table->string('minim_qualification');
+            $table->string('experience_level');
+            $table->string('experience_length');
+            $table->string('candidates')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
---
+++ database/migrations/2025_02_02_093815_create_job_applies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_applies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('job_id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('location');
+            $table->string('phone_number');
+            $table->string('cv');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_applies');
+    }
+};
