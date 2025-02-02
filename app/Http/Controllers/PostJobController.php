702632aa4c77e7f1110817b91fb4f501d2988884
EfiokobongN <mailpreciousnelson@gmail.com>
2025-02-02 16:46:42 +0000

    job details edited

--- app/Http/Controllers/PostJobController.php
+++ app/Http/Controllers/PostJobController.php
@@ -43,4 +43,41 @@ class PostJobController extends Controller
         }
         return $response;
     }
+
+
+    //Edit Job Dateils Function Method
+    public function editJob(PostJobRequest $request,$id)
+    {
+        $user = Util::Auth();
+        $job = Job::find($id);
+        if (!$job || $job->user_id!= $user->id) {
+            return response()->json(['success'=>false, 'error' => 'Job not found or not belong to you'], 404);
+        }
+        $data = $request->all();
+        $dateFormatted = Carbon::createFromFormat('d/m/Y', $data['submission_deadline'])->format('Y-m-d');
+        // Perform database operations to update the job data
+        DB::beginTransaction();
+
+        try {
+            $job->job_title = $data['job_title'];
+            $job->company_name = $data['company_name'];
+            $job->job_description = $data['job_description'];
+            $job->location = $data['location'];
+            $job->employment_status = $data['employment_status'];
+            $job->salary_range = $data['salary_range'];
+            $job->submission_deadline = $dateFormatted;
+            $job->job_sector = $data['job_sector'];
+            $job->minim_qualification = $data['minim_qualification'];
+            $job->experience_level = $data['experience_level'];
+            $job->experience_length = $data['experience_length'];
+            $job->job_requirements = $data['job_requirements'];
+            $job->update();
+            DB::commit();
+            $response = response()->json(['success'=>true, 'message' => 'Job updated successfully', 'jobdetails'=>$job], 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            $response = response()->json(['success'=>false, 'error' => $th->getMessage()], 500);
+        }
+        return $response;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function (){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/my/jobs', [PostJobController::class, 'storeJob']);
+    Route::post('/my/jobs/{id}', [PostJobController::class, 'editJob']);
 });
