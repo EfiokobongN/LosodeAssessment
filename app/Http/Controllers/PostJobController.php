89983e988bb90eb6c9b8ffd44fd6f22a5635bfc6
EfiokobongN <mailpreciousnelson@gmail.com>
2025-02-03 09:10:23 +0000

    postJob and EditJob refactor function

--- app/Http/Controllers/PostJobController.php
+++ app/Http/Controllers/PostJobController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ApplyJobRequest;
 use App\Http\Requests\PostJobRequest;
 use App\Models\Job;
 use App\Models\JobApply;
+use App\Services\JobService;
 use App\Utility\Util;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -13,30 +14,18 @@ use Illuminate\Support\Facades\DB;
 
 class PostJobController extends Controller
 {
+    protected $jobservice;
+    public function __construct(JobService $jobservice)
+    {
+        $this->jobservice = $jobservice;
+    }
     public function storeJob(PostJobRequest $request)
     {
-        $user = Util::Auth();
         $data = $request->validated();
-        $dateFormatted = Carbon::createFromFormat('d/m/Y', $data['submission_deadline'])->format('Y-m-d');
         // Perform database operations to store the job data
         DB::beginTransaction();
-
         try {
-            $postJob = new Job();
-            $postJob->user_id = $user->id;
-            $postJob->job_title = $data['job_title'];
-            $postJob->company_name = $data['company_name'];
-            $postJob->job_description = $data['job_description'];
-            $postJob->location = $data['location'];
-            $postJob->employment_type = $data['employment_type'];
-            $postJob->salary_range = $data['salary_range'];
-            $postJob->submission_deadline = $dateFormatted;
-            $postJob->category = $data['category'];
-            $postJob->minim_qualification = $data['minim_qualification'];
-            $postJob->experience_level = $data['experience_level'];
-            $postJob->experience_length = $data['experience_length'];
-            $postJob->job_requirements = $data['job_requirements'];
-            $postJob->save();
+            $postJob = $this->jobservice->storeJob($data);
             DB::commit();
             $response = response()->json(['success'=>true, 'message' => 'Job posted successfully', 'jobdetails'=>$postJob], 201);
         } catch (\Throwable $th) {
@@ -48,34 +37,15 @@ class PostJobController extends Controller
 
 
     //Edit Job Dateils Function Method
-    public function editJob(PostJobRequest $request,$id)
+    public function editJob(PostJobRequest $request,$jobId)
     {
-        $user = Util::Auth();
-        $job = Job::find($id);
-        if (!$job || $job->user_id!= $user->id) {
-            return response()->json(['success'=>false, 'error' => 'Job not found or not belong to you'], 404);
-        }
-        $data = $request->all();
-        $dateFormatted = Carbon::createFromFormat('d/m/Y', $data['submission_deadline'])->format('Y-m-d');
+        $data = $request->validated();
         // Perform database operations to update the job data
         DB::beginTransaction();
-
         try {
-            $job->job_title = $data['job_title'];
-            $job->company_name = $data['company_name'];
-            $job->job_description = $data['job_description'];
-            $job->location = $data['location'];
-            $job->employment_type = $data['employment_type'];
-            $job->salary_range = $data['salary_range'];
-            $job->submission_deadline = $dateFormatted;
-            $job->category = $data['category'];
-            $job->minim_qualification = $data['minim_qualification'];
-            $job->experience_level = $data['experience_level'];
-            $job->experience_length = $data['experience_length'];
-            $job->job_requirements = $data['job_requirements'];
-            $job->update();
+            $editJob = $this->jobservice->edit($jobId,$data);
             DB::commit();
-            $response = response()->json(['success'=>true, 'message' => 'Job updated successfully', 'jobdetails'=>$job], 200);
+            $response = response()->json(['success'=>true, 'message' => 'Job updated successfully', 'jobdetails'=>$editJob], 200);
         } catch (\Throwable $th) {
             DB::rollBack();
             $response = response()->json(['success'=>false, 'error' => $th->getMessage()], 500);
